6c26bf2c902c9efafa420ecf3331327269317098
harveyconnor <harveyconnor@icloud.com>
2017-01-30 14:31:11 +1100

    Favicon Added

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>AusExile</title>
+    <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}">
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
     <link href="/js/style.css" rel="stylesheet">
     <script>
