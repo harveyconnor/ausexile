e8d8ad10a89ecbd8f4e593a62e882f5dfca7c311
harveyconnor <harveyconnor@icloud.com>
2017-01-30 17:35:16 +1100

    Widgets Fix
    Updated

--- resources/views/partials/widgets.blade.php
+++ resources/views/partials/widgets.blade.php
@@ -45,6 +45,33 @@
 @section('above_scripts')
     <script src="{{ asset('/js/clipboard.min.js') }}"></script>
     <script>
-        new Clipboard('#ip-copy');
+        $('#ip-copy').tooltip({
+            trigger: 'click',
+            placement: 'bottom'
+        });
+
+        function setTooltip(btn, message) {
+            $(btn).tooltip('hide')
+                .attr('data-original-title', message)
+                .tooltip('show');
+        }
+
+        function hideTooltip(btn) {
+            setTimeout(function() {
+                $(btn).tooltip('hide');
+            }, 1000);
+        }
+
+        var clipboard = new Clipboard('#ip-copy');
+
+        clipboard.on('success', function(e) {
+            setTooltip(e.trigger, 'Copied!');
+            hideTooltip(e.trigger);
+        });
+
+        clipboard.on('error', function(e) {
+            setTooltip(e.trigger, 'Failed!');
+            hideTooltip(e.trigger);
+        });
     </script>
 @endsection
