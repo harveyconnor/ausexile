2c38245f56b2f169501dedc127e2f7df8ac3c1ee
harveyconnor <harveyconnor@icloud.com>
2017-01-30 18:03:03 +1100

    Updated Head

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,7 +5,7 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>AusExile</title>
+    <title>AusExile - Conan Exiles</title>
     <link rel="shortcut icon" href="{{ asset('/favicon.png') }}">
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
     <link href="/js/style.css" rel="stylesheet">
